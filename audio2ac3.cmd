013e019bcd60df57212eae2f66403f8e556a46aa
agarsab <agarsab@gmail.com>
2022-10-02 20:17:19 +0100

    Allow a single file or all files in a folder
    Add license info

--- audio2ac3.cmd
+++ audio2ac3.cmd
@@ -1,13 +1,34 @@
 @echo off
-:: Use ffmpeg to convert any audio stream to Dolby Digital or AC-3
-:: ffmpeg.exe has to be in the same directory or in PATH environment variable
+#================================================================
+# AUTHOR        Angel Garcia-Galan (angelgs@gmail.com)
+# LICENSE       European Union Public Licence (EUPL)
+#               https://joinup.ec.europa.eu/collection/eupl/
+# SOURCE        https://github.com/agarsab/ffmpeg
+# FUNCTION      Use ffmpeg to convert any audio stream to Dolby Digital or AC-3
+# NOTES         ffmpeg.exe has to be in the same directory or in PATH environment variable
+#               Tested in Microsoft Windows 10
+# ARGUMENTS     -f filename
+#               -d foldername
+#================================================================
 
-set input_file=%1
-set output_file=%~n1[AC3]%~x1
+IF /I "%~1" == "-f" goto File
+IF /I "%~1" == "-d" goto Directory
 
+::Usage
+echo Usage: %0 -f filename.ext
+echo Usage: %0 -d foldername
+exit /b 1
+
+:File
+set input_file=%2
+set output_file=%~n2[AC3]%~x2
 IF EXIST %input_file% (
     ffmpeg.exe -hide_banner -i %input_file% -map 0 -vcodec copy -scodec copy -acodec ac3 -b:a 640k "%output_file%"
+    exit /b 0
 ) ELSE (
-    echo Usage: %0 input_file_name.ext
     echo Error: File %input_file% not foud
-)
+    exit /b 2)
+
+:Directory
+forfiles /p %2 /c "cmd /c ffmpeg.exe -hide_banner -i @file -map 0 -vcodec copy -scodec copy -acodec ac3 -b:a 640k @fname[AC3].@ext"
+exit /b 0
